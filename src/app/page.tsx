89511d7a9f03e2be84533f0b5b7894903c7988e1
Ramez <ramezyouakimf@gmail.com>
2024-08-27 17:35:58 +0300

    add table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,8 +89,9 @@ export default function Home() {
       ) : (
         <h1>Loading ...</h1>
       )}
-
-      <h1>new changes 2</h1>
+      {/* <h1>Buy physical goods</h1>
+      <button onClick={openInvoice} type="button" className="flex-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 self-center">pay</button> */}
+      <h1>Buy digital goods</h1>
       <button onClick={openInvoice} type="button" className="flex-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 self-center">pay</button>
     </div>
   );
