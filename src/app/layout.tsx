bc54e7bfe5395a678a4b181273117e79324b0254
Ramez <ramezyouakimf@gmail.com>
2024-09-12 16:32:32 +0300

    addchecker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,14 +20,6 @@ export default function RootLayout({
       <head>
         <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
       </head>
-      <script
-      async
-      src="https://telegram.org/js/telegram-widget.js?22"
-      data-telegram-login="test2314nBot"
-      data-size="large"
-      data-auth-url="https://test-n55s.vercel.app/"
-      data-request-access="write"
-    ></script>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,111 +1,31 @@
 "use client"
+import { useEffect } from "react";
+import Script from "next/script";
 
-import WebApp from "@twa-dev/sdk";
-import { useEffect, useState } from "react";
-import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react';
+declare global {
+  interface Window {
+    ReactNativeWebView: {
+      postMessage: (message: any) => void;
+    };
+  }
+}
 
 export default function Home() {
-  const [userData, setUserData] = useState<null | any>(null)
-  const [data, setData] = useState<null | any>(null)
-
-
   useEffect(() => {
     if (typeof window !== "undefined") {
       const searchParams = new URLSearchParams(window.location.search);
-      alert(searchParams)
-    }
-  }, [])
+      const paramsObject = Object.fromEntries(searchParams.entries());
 
-  useEffect(() => {
-    if (WebApp.initDataUnsafe?.user) {
-      setUserData(WebApp.initDataUnsafe.user)
-    }
-  }, [])
-
-  const openInvoice = async () => {
-    try {
-      const response = await fetch("https://api.telegram.org/bot7529380285:AAHj4qaLHkTo6IWfYWHXqspZ29fn05Wq_BI/createInvoiceLink", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          title: 'test item',
-          description: 'test description',
-          payload: 'unique_payload_identifier',
-          provider_token: '',
-          currency: 'XTR',
-          prices: [
-            {
-              label: 'Breakfast included',
-              amount: 1000,
-            },
-          ],
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+      if (window.ReactNativeWebView) {
+        window.ReactNativeWebView.postMessage(JSON.stringify(paramsObject));
       }
-
-      const data = await response.json();
-      console.log('Invoice created:', data);
-
-      if (typeof window !== 'undefined' && WebApp) {
-        WebApp.openInvoice(data?.result);
-      }
-      console.log("result ", data?.result);
-    } catch (error) {
-      console.error('Error creating invoice:', error);
+      window.parent.postMessage(paramsObject)
     }
-
-    setData(data);
-  };
+  }, []);
 
   return (
-    <TonConnectUIProvider manifestUrl="https://test-n55s.vercel.app/tonconnect-manifest.json">
-      <div className="flex-1 justify-center items-center">
-        {userData ? (
-          <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    name
-                  </th>
-                  <td className="px-6 py-4">
-                    {userData.first_name} {userData.last_name}
-                  </td>
-                </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    ID
-                  </th>
-                  <td className="px-6 py-4">
-                    {userData.id}
-                  </td>
-                </tr>
-                <tr className="bg-white dark:bg-gray-800">
-                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Is premium
-                  </th>
-                  <td className="px-6 py-4">
-                    {userData.is_premium || 'false'}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        ) : (
-          <h1>Loading ...</h1>
-        )}
-        {/* <h1>Buy physical goods</h1>
-      <button onClick={openInvoice} type="button" className="flex-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 self-center">pay</button> */}
-        <h1>Buy digital goods</h1>
-        <button onClick={openInvoice} type="button" className="flex-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 self-center">pay</button>
-        <TonConnectButton />
-
-      </div>
-    </TonConnectUIProvider>
+    <div>
+<p>dksdksd</p>
+    </div>
   );
 }
